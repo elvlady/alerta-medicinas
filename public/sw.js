938a92084742a43713b46f8664f38c24a61b072e
vladimirsanchez <vladyoz@gmail.com>
2026-05-24 04:58:41 -0700

    Make treatment buttons green

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const APP_CACHE = "alerta-medicinas-v47";
+const APP_CACHE = "alerta-medicinas-v48";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/manifest.webmanifest?v=47",
-  "/styles.css?v=47",
-  "/app.js?v=47",
+  "/manifest.webmanifest?v=48",
+  "/styles.css?v=48",
+  "/app.js?v=48",
   "/iconos/logo.png",
   "/iconos/bell.svg",
   "/iconos/android/mipmap-hdpi/ic_launcher.png",
